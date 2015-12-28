40e4b2ad12aa88b2149943c829bf3092947ee9b5
beliefs22 <beliefs22@gmail.com>
2015-12-28 14:16:54 -0500

    I still hate match

--- recursivelist.py
+++ recursivelist.py
@@ -8,40 +8,85 @@ def recursive_add(mylist, total = []):
         
         return recursive_add(
             mylist[1:], total + [mylist[0]*num for num in  mylist[1:]])
-def recursive_factor(num, prime_list):
-    print "number is", num
+
+def recursive_perms(sequence):
+    if len(sequence) == 0:
+        raise Exception("list must be > 0")
+    if len(sequence) == 1:
+        return {tuple(sequence):1}
+
+    #get all permutations of length N-1
+    all_perms = recursive_perms(sequence[1:])
+    insert = [sequence[0]]
+    result = {}
+    for perm in all_perms.keys():
+        perm = list(perm)
+        for i in range(len(perm) + 1):
+            permutation = perm[:i] + insert + perm[i:]
+            result[tuple(permutation)] = result.get(tuple(permutation),0) + 1
+    return result
+def recursive_combs(sequence):
+    #expects list?
+    results = {}
+    for i in range(1,len(sequence)+1): # creates [] --> len(seq)*[]
+        combs = split_sequence(sequence,i)
+        for comb in combs:
+            results[comb] = results.get(comb, 0) + 1 #only add uniqe results
+    return results
+
+def split_sequence(sequence,size):
+    """Creates max number olist of specified size from sequence in given order.
+
+    """
+    results = {}
+    i = 0
+    #run until size becomes greater than size of sequence
+    while i + size < len(sequence)+1: 
+        split = sequence[i:i+size]
+        results[split] = results.get(split,0) + 1
+        i += 1
+    return results
+    
+def prime_factors(num, prime_list):
     possible_primes = prime_list
-    prime_factors = []
+    prime_factors = [1]
     done = False
     temp = num
     if num == 0:
         return []
     if num == 1:
-        return [1]
+        return ['1']
     if num in possible_primes:
-        return [1,num]
+        return ['1',str(num)]
     while  temp != 1 and temp != 0:        
         for prime in possible_primes:
-            print temp,prime ,"temp is prime is"
             if temp % prime == 0:
-                prime_factors.append(prime)
+                prime_factors.append(str(prime))
                 temp = temp/prime
                 break
     return prime_factors
+
+def factors(num,prime_list):
+    all_perms = recursive_perms(prime_factors(num,prime_list))
+    factors = {}
+    for perm in all_perms.keys():
+        possible_combs = recursive_combs(perm)
+        for comb in possible_combs.keys():
+            factor = eval("*".join([str(num) for num in comb]))
+            factors[factor] = factors.get(factor, 0) +1
+    return sorted(factors.keys())
 def main():
     number = 150000000
     p = prime.Prime()
     prime_list = p.gen_prime_list()
-    prime_factors = recursive_factor(number, prime_list)
-    possible_factors = list(set(recursive_add(prime_factors)))
-    distinct_prime_factors = list(set(prime_factors))
-    all_factors = [a*b for a in distinct_prime_factors \
-                   for b in possible_factors]
-    all_factors = all_factors + distinct_prime_factors+possible_factors+[1,number]
-    print sorted(list(set(all_factors))), "all factors?"
-    print prime_factors, distinct_prime_factors, "prime factors"
-    print sorted(possible_factors), "factors found recursively"
-    print sorted(list(set(recursive_add(all_factors))))
+    for i in range(1,1000000):
+         a = prime_factors(i, prime_list)
+         if len(set(a)) > 7:
+             print a
+             print "found one", i
+
+    print "done"
+        
 
 
 if __name__=='__main__':
